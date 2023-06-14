d7633975b7ddfb34b731d4eb3cb68b675c45ec2d
Edmond Serato <alphashadow0418@gmail.com>
2023-06-14 10:15:05 +0800

    added

---
+++ head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+<link rel="stylesheet" href="heading.css">
+</head>
+<body>
+  <container class="contain">
+    <div class="space"></div>  
+    <div>
+      <div class="ediths">Ediths</div>
+      <div class="catering">Catering</div>
+    </div>
+    <div class="space"></div>
+    <div class="home menu">HOME</div>
+    <div class="services menu">SERVICES</div>
+    <div class="menu menu">MENU</div>
+    <div class="eventPlace menu">EVENT PLACE</div>
+    <div class="book menu">BOOK</div>
+    <div class="contactUs menu">CONTACT US</div>
+    <div class="space"></div>
+  </container>
+  <div class="hRuler"> </div>
+</body>
+</html>
